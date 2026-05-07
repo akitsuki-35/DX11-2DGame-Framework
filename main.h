a9f740ea1edac25d65e0a05e75e536439ca93d80
あきつき <asukah79@gmail.com>
2026-05-07 12:34:51 +0900

    add: main.hにカラー・スケール定数の定義を追加

--- main.h
+++ main.h
@@ -10,21 +10,55 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*----------------------------------------------------------------------------------------------------------
+#include <DirectXMath.h>
+
+/*＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝
 	定数定義
+＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝*/
+/*----------------------------------------------------------------------------------------------------------
+	スクリーン関連パラメータ定義
 ----------------------------------------------------------------------------------------------------------*/
-// 画面解像度
-static constexpr int SCREEN_WIDTH{ 1920 };
-static constexpr int SCREEN_HEIGHT{ 1080 };
+namespace Screen {
+	// 画面解像度
+	static constexpr int WIDTH { 1920 };
+	static constexpr int HEIGHT{ 1080 };
 
-// X・Y中心座標
-static constexpr int SCREEN_CENTER_X{ SCREEN_WIDTH / 2 };
-static constexpr int SCREEN_CENTER_Y{ SCREEN_HEIGHT / 2 };
+	// X・Y中心座標
+	static constexpr int CENTER_X{ WIDTH / 2 };
+	static constexpr int CENTER_Y{ HEIGHT / 2 };
 
-// スクリーン端座標
-static constexpr int SCREEN_TOP{ 0 };
-static constexpr int SCREEN_BOTTOM{ SCREEN_HEIGHT };
-static constexpr int SCREEN_LEFT{ 0 };
-static constexpr int SCREEN_RIGHT{ SCREEN_WIDTH };
+	// スクリーン端座標
+	static constexpr int TOP	{ 0 };
+	static constexpr int BOTTOM { HEIGHT };
+	static constexpr int LEFT	{ 0 };
+	static constexpr int RIGHT  { WIDTH };
+}
+
+/*----------------------------------------------------------------------------------------------------------
+	簡易カラーパレット定義
+----------------------------------------------------------------------------------------------------------*/
+namespace Color {
+	static constexpr DirectX::XMFLOAT4 WHITE   { 1.0f, 1.0f, 1.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 BLACK   { 0.0f, 0.0f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 GRAY	   { 0.0f, 0.0f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 RED	   { 1.0f, 0.0f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 GREEN   { 0.0f, 1.0f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 BLUE	   { 0.0f, 0.0f, 1.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 YELLOW  { 1.0f, 1.0f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 CYAN	   { 0.0f, 1.0f, 1.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 PURPLE  { 1.0f, 0.0f, 1.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 ORANGE  { 1.0f, 0.5f, 0.0f, 1.0f };
+	static constexpr DirectX::XMFLOAT4 MAGENTA { 1.0f, 0.0f, 0.5f, 1.0f };
+}
+
+/*----------------------------------------------------------------------------------------------------------
+	スケール定数定義
+----------------------------------------------------------------------------------------------------------*/
+namespace Scale {
+	static constexpr float DEFAULT { 1.0f };
+	static constexpr float HALF	   { 0.5f };
+	static constexpr float DOUBLE  { 2.0f };
+	static constexpr float QUADRUPE{ 4.0f };
+}
 
 #endif // MAIN_H
