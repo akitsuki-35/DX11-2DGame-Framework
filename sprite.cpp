c17f3236faa98c44fc630c34604549fbcd59620c
あきつき <asukah79@gmail.com>
2026-05-14 17:44:23 +0900

    add: refactor: テスト用テクスチャを追加,

--- sprite.cpp
+++ sprite.cpp
@@ -17,6 +17,8 @@ using namespace DirectX;
 static constexpr int NUM_VERTEX{ 4 }; // 頂点数
 static ID3D11Buffer* g_pVertexBuffer{ nullptr }; // 頂点バッファ
 
+//ID3D11Buffer* Sprite::pVertexBuffer{ nullptr }; // 頂点バッファ
+
 // 頂点構造体
 struct Vertex
 {
@@ -545,3 +547,172 @@ void SpriteDraw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position, co
 	// ポリゴン描画命令発行
 	Direct3DGetDeviceContext()->Draw(NUM_VERTEX, 0);
 }
+
+// クラス化
+// 仮
+//void Sprite::Initialize()
+//{
+//	// 頂点バッファ生成
+//	D3D11_BUFFER_DESC bd = {};
+//	bd.Usage = D3D11_USAGE_DYNAMIC;
+//	bd.ByteWidth = sizeof(Vertex) * NUM_VERTEX;
+//	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+//	bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+//
+//	Direct3DGetDevice()->CreateBuffer(&bd, NULL, &pVertexBuffer);
+//}
+//
+//void Sprite::Finalize()
+//{
+//	SAFE_RELEASE(pVertexBuffer);
+//}
+//
+//void Sprite::Draw(Texture* pTexture, const DirectX::XMFLOAT2& position, const DirectX::XMFLOAT2& size, const float& angle, const DirectX::XMFLOAT4& color)
+//{
+//	pTexture->SetTexture();
+//
+//	// シェーダーを描画パイプラインに設定
+//	Shader2DBeginLinear();
+//
+//	// 頂点バッファをロックする
+//	D3D11_MAPPED_SUBRESOURCE msr;
+//	Direct3DGetDeviceContext()->Map(pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
+//
+//	// 頂点バッファへの仮想ポインタを取得
+//	Vertex* v = (Vertex*)msr.pData;
+//
+//	// 頂点情報を書き込み
+//	const float SCREEN_WIDTH = static_cast<float>(Direct3DGetBackBufferWidth());
+//	const float SCREEN_HEIGHT = static_cast<float>(Direct3DGetBackBufferHeight());
+//
+//	//四角形の描画
+//	v[0].position = { position.x, position.y, 0.0f };
+//	v[1].position = { position.x + size.x, position.y, 0.0f };
+//	v[2].position = { position.x, position.y + size.y, 0.0f };
+//	v[3].position = { position.x + size.x, position.y + size.y, 0.0f };
+//
+//	for (int i = 0; i < NUM_VERTEX; i++)
+//	{
+//		v[i].color = color;
+//	}
+//
+//	v[0].texCoord = { 0.0f, 0.0f };
+//	v[1].texCoord = { 1.0f, 0.0f };
+//	v[2].texCoord = { 0.0f, 1.0f };
+//	v[3].texCoord = { 1.0f, 1.0f };
+//
+//	// 頂点バッファのロックを解除
+//	Direct3DGetDeviceContext()->Unmap(pVertexBuffer, 0);
+//
+//	// 頂点バッファを描画パイプラインに設定
+//	UINT stride = sizeof(Vertex);
+//	UINT offset = 0;
+//	Direct3DGetDeviceContext()->IASetVertexBuffers(0, 1, &pVertexBuffer, &stride, &offset);
+//
+//	// 中心座標を算出
+//	float centerX = position.x + size.x / 2;
+//	float centerY = position.y + size.y / 2;
+//
+//	// 中心座標に平行移動→回転→元の座標に平行移動
+//	XMMATRIX transToCenter = XMMatrixTranslation(-centerX, -centerY, 0.0f);
+//	XMMATRIX rot = XMMatrixRotationZ(-angle);
+//	XMMATRIX transBack = XMMatrixTranslation(centerX, centerY, 0.0f);
+//
+//	// 頂点シェーダーにワールド変換行列を設定
+//	Shader2DSetWorldMatrix(transToCenter * rot * transBack);
+//
+//	// 頂点シェーダーにプロジェクション変換行列を設定
+//	Shader2DSetProjectionMatrix(XMMatrixOrthographicOffCenterLH(0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f, 0.0f, 1.0f));
+//	Shader2DSetColor({ 1.0f,1.0f,1.0f,1.0f });
+//
+//	// プリミティブトポロジ設定
+//	Direct3DGetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+//
+//	// ポリゴン描画命令発行
+//	Direct3DGetDeviceContext()->Draw(NUM_VERTEX, 0);
+//}
+//
+//void Sprite::Draw(Texture* pTexture, const DirectX::XMFLOAT2& position, const float& angle, const float& scale, const DirectX::XMFLOAT4& color)
+//{
+//	pTexture->SetTexture();
+//
+//	// シェーダーを描画パイプラインに設定
+//	Shader2DBeginLinear();
+//
+//	// 頂点バッファをロックする
+//	D3D11_MAPPED_SUBRESOURCE msr;
+//	Direct3DGetDeviceContext()->Map(pVertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &msr);
+//
+//	// 頂点バッファへの仮想ポインタを取得
+//	Vertex* v = (Vertex*)msr.pData;
+//
+//	// 頂点情報を書き込み
+//	const float SCREEN_WIDTH = static_cast<float>(Direct3DGetBackBufferWidth());
+//	const float SCREEN_HEIGHT = static_cast<float>(Direct3DGetBackBufferHeight());
+//
+//	// テクスチャのサイズ取得
+//	float texWidth = static_cast<float>(pTexture->GetSize().x);
+//	float texHeight = static_cast<float>(pTexture->GetSize().y);
+//
+//	//四角形の描画
+//	v[0].position = { position.x, position.y, 0.0f };
+//	v[1].position = { position.x + (texWidth * scale), position.y, 0.0f };
+//	v[2].position = { position.x, position.y + (texHeight * scale), 0.0f };
+//	v[3].position = { position.x + (texWidth * scale), position.y + (texHeight * scale), 0.0f };
+//
+//	for (int i = 0; i < NUM_VERTEX; i++)
+//	{
+//		v[i].color = color;
+//	}
+//
+//	v[0].texCoord = { 0.0f, 0.0f };
+//	v[1].texCoord = { 1.0f, 0.0f };
+//	v[2].texCoord = { 0.0f, 1.0f };
+//	v[3].texCoord = { 1.0f, 1.0f };
+//
+//	// 頂点バッファのロックを解除
+//	Direct3DGetDeviceContext()->Unmap(pVertexBuffer, 0);
+//
+//	// 頂点バッファを描画パイプラインに設定
+//	UINT stride = sizeof(Vertex);
+//	UINT offset = 0;
+//	Direct3DGetDeviceContext()->IASetVertexBuffers(0, 1, &pVertexBuffer, &stride, &offset);
+//
+//	// 中心座標を算出
+//	float centerX = position.x + (texWidth * scale) / 2;
+//	float centerY = position.y + (texHeight * scale) / 2;
+//
+//	// 中心座標に平行移動→回転→元の座標に平行移動
+//	XMMATRIX transToCenter = XMMatrixTranslation(-centerX, -centerY, 0.0f);
+//	XMMATRIX rot = XMMatrixRotationZ(-angle);
+//	XMMATRIX transBack = XMMatrixTranslation(centerX, centerY, 0.0f);
+//
+//	// 頂点シェーダーにワールド変換行列を設定
+//	Shader2DSetWorldMatrix(transToCenter * rot * transBack);
+//
+//	// 頂点シェーダーにプロジェクション変換行列を設定
+//	Shader2DSetProjectionMatrix(XMMatrixOrthographicOffCenterLH(0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f, 0.0f, 1.0f));
+//	Shader2DSetColor({ 1.0f,1.0f,1.0f,1.0f });
+//
+//	// プリミティブトポロジ設定
+//	Direct3DGetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+//
+//	// ポリゴン描画命令発行
+//	Direct3DGetDeviceContext()->Draw(NUM_VERTEX, 0);
+//}
+
+//void Sprite::Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position, const DirectX::XMUINT2& patternNum, DirectX::XMFLOAT2 size, const float& angle, const DirectX::XMFLOAT4& color)
+//{
+//}
+//
+//void Sprite::Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position, const DirectX::XMUINT2& patternNum, const float& angle, const float& scale, const DirectX::XMFLOAT4& color)
+//{
+//}
+//
+//void Sprite::Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position, const int& patternNum, DirectX::XMFLOAT2 size, const float& angle, const DirectX::XMFLOAT4& color)
+//{
+//}
+//
+//void Sprite::Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position, const int& patternNum, const float& angle, const float& scale, const DirectX::XMFLOAT4& color)
+//{
+//}
--- sprite.h
+++ sprite.h
@@ -15,6 +15,54 @@
 class Texture;
 class SpriteSheet;
 
+//class Sprite
+//{
+//private:
+//	static constexpr int NUM_VERTEX{ 4 }; // 頂点数
+//	static ID3D11Buffer* pVertexBuffer; // 頂点バッファ
+//
+//public:
+//	static void Initialize();
+//	static void Finalize();
+//
+//	/*----------------------------------------------------------------------------------------------------------
+//		通常スプライト描画
+//	----------------------------------------------------------------------------------------------------------*/
+//	// 数値で直接サイズ指定
+//	static void Draw(Texture* pTexture, const DirectX::XMFLOAT2& position, const DirectX::XMFLOAT2& size,
+//		const float& angle = 0.0f, const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//
+//	// 拡大率でサイズ指定
+//	static void Draw(Texture* pTexture, const DirectX::XMFLOAT2& position, const float& angle = 0.0f,
+//		const float& scale = 1.0f, const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//
+//	/*----------------------------------------------------------------------------------------------------------
+//		0を始点としたx, y番号でパターン指定
+//	----------------------------------------------------------------------------------------------------------*/
+//	// 数値で直接サイズ指定
+//	static void Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position,
+//		const DirectX::XMUINT2& patternNum, DirectX::XMFLOAT2 size, const float& angle = 0.0f,
+//		const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//
+//	// 拡大率でサイズ指定
+//	static void Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position,
+//		const DirectX::XMUINT2& patternNum, const float& angle = 0.0f, const float& scale = 1.0f,
+//		const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//
+//	/*----------------------------------------------------------------------------------------------------------
+//		0を始点としたパターン番号でパターン指定
+//	----------------------------------------------------------------------------------------------------------*/
+//	// 数値で直接サイズ指定
+//	static void Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position,
+//		const int& patternNum, DirectX::XMFLOAT2 size, const float& angle = 0.0f,
+//		const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//
+//	// 拡大率でサイズ指定
+//	static void Draw(SpriteSheet* pSpriteSheet, const DirectX::XMFLOAT2& position,
+//		const int& patternNum, const float& angle = 0.0f, const float& scale = 1.0f,
+//		const DirectX::XMFLOAT4& color = { 1.0f,1.0f,1.0f,1.0f });
+//};
+
 void SpriteInitialize();
 void SpriteFinalize();
 
